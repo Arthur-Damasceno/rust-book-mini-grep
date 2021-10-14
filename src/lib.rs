461765b2657aebba573d2551cfb854bcac250b2e
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-10-14 16:54:11 -0300

    feat: reading file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
@@ -6,5 +6,10 @@ pub fn run() {
     let query = &args[1];
     let filename = &args[2];
 
-    println!("Searching for: {}\nIn file: {}", query, filename);
+    println!("Searching for: {}\nIn file: {}\n", query, filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With the context:\n{}", contents)
 }
